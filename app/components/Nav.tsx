41a83e9ce89c183791bd468042475797a9482618
HireMeFullTime <2017pracownik@gmail.com>
2024-07-13 19:55:49 +0200

    Added active link styles for tablet&desktop

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,17 +1,25 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { navLinks } from '../utils/navllinks';
 
 const Nav = () => {
+  const pathname = usePathname();
+
   return (
-    <div className="relative hidden  h-full w-full max-w-[450px] items-center bg-[#FFFFFF0A] px-12 font-barlow_condensed text-sm tracking-[2.36px] text-white   backdrop-blur-2xl md:flex lg:max-w-[830px] xl:pl-[123px] xl:text-base xl:tracking-[2.7px]">
-      <ul className="flex gap-9">
+    <div className="hidden  h-full w-full max-w-[450px] items-center bg-[#FFFFFF0A] px-12 font-barlow_condensed text-sm tracking-[2.36px] text-white   backdrop-blur-2xl md:flex lg:max-w-[830px] xl:pl-[123px] xl:text-base xl:tracking-[2.7px]">
+      <ul className="flex h-full gap-9 ">
         {navLinks.map((item) => (
-          <li key={item.id} className="">
+          <li key={item.id} className=" relative flex h-full items-center">
             <Link href={item.href} className="flex gap-3">
-              <span className="hidden font-bold lg:block">{item.id}</span>{' '}
+              <span className="hidden font-bold lg:block">{item.id}</span>
               {item.name}
             </Link>
+            {pathname === item.href && (
+              <div className="absolute bottom-0 left-0 right-0 h-[3px] bg-white"></div>
+            )}
           </li>
         ))}
       </ul>
