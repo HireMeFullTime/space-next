eb6c20a741d9f430acdec3c467c7faeb51df73a3
HireMeFullTime <2017pracownik@gmail.com>
2024-07-15 11:34:23 +0200

    Added favicon & styles for nav background (for browsers not supporting filter)

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -9,8 +9,8 @@ const Nav = () => {
   const pathname = usePathname();
 
   return (
-    <div className="hidden  h-full w-full max-w-[450px] items-center bg-[#FFFFFF0A] px-12 font-barlow_condensed text-sm tracking-[2.36px] text-white   backdrop-blur-2xl md:flex lg:max-w-[830px] xl:pl-[123px] xl:text-base xl:tracking-[2.7px]">
-      <ul className="flex h-full gap-9 ">
+    <nav className="hidden  h-full w-full max-w-[450px] items-center bg-[#FFFFFF0A] px-12 font-barlow_condensed text-sm tracking-[2.36px] text-white   outline-dotted outline-1 outline-orange-300 backdrop-blur-2xl supports-[not(backdrop-filter:blur(1px))]:bg-[#1c1a26] md:flex lg:max-w-[830px] xl:pl-[123px] xl:text-base xl:tracking-[2.7px]">
+      <ul className="relative flex h-full gap-9 ">
         {navLinks.map((item) => (
           <li key={item.id} className="relative flex h-full items-center">
             <Link href={item.href} className="peer flex gap-3">
@@ -25,9 +25,9 @@ const Nav = () => {
             )}
           </li>
         ))}
+        <div className="absolute  -left-[570px] top-2/4 hidden h-[1px] w-full max-w-[473px] bg-[#ffffff40] xl:block" />
       </ul>
-      <div className="absolute -left-[443px] top-2/4 hidden h-[1px] w-full max-w-[473px] bg-[#ffffff40] xl:block" />
-    </div>
+    </nav>
   );
 };
 export default Nav;
