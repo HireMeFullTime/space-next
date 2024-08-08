d950956d63db21ee8113b40b2a754472648d0493
HireMeFullTime <2017pracownik@gmail.com>
2024-08-08 22:21:49 +0200

    Ctreated && styled home page

--- app/components/HamburgerMenuItems.tsx
+++ app/components/HamburgerMenuItems.tsx
@@ -12,7 +12,7 @@ interface MenuItemsProps {
 const HamburgerMenuItems: React.FC<MenuItemsProps> = ({ closeMenu }) => {
   const pathname = usePathname();
   return (
-    <nav className="animate-slide-in fixed right-0 top-0 z-50 flex h-screen w-64 max-w-64 flex-col gap-16 overflow-y-auto bg-white/5 pl-8 pr-0 pt-8 text-white  backdrop-blur-xl supports-[not(backdrop-filter:blur(1px))]:bg-[#1c1a26]">
+    <nav className="animate-slide-in fixed right-0 top-0 z-[100] flex h-screen w-64 max-w-64 flex-col gap-16 overflow-y-auto bg-white/5 pl-8 pr-0 pt-8 text-white  backdrop-blur-xl supports-[not(backdrop-filter:blur(1px))]:bg-[#1c1a26]">
       <button onClick={closeMenu} className="self-end pr-7">
         <Image src={close_icon} alt="Close icon" width={20} height={20} />
       </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import Link from 'next/link';
+
 const Home = () => {
   return (
-    <div className="flex w-full  flex-col overflow-y-auto ">
-      <p>Home</p>
+    <div className="flex  w-full flex-col items-center overflow-y-auto pt-6 text-white md:pt-16 xl:flex-row xl:items-end xl:gap-[400px] xl:pb-32 xl:pt-0 ">
+      <div className="xl: flex flex-col xl:h-full xl:max-h-96 xl:w-[450px] xl:justify-between">
+        <h1 className="flex flex-col text-center font-barlow_condensed uppercase leading-5 tracking-[2.7px] text-[#D0D6F9] md:text-xl md:tracking-[3.38px] xl:text-left xl:text-[28px] xl:tracking-[4.72px] ">
+          So, you want to travel to
+          <span className="py-4 font-bellefair text-[80px] leading-[105px] text-white md:py-10 md:text-[150px] xl:mt-5">
+            Space
+          </span>
+        </h1>
+        <p className="mb-20 text-center font-barlow text-[15px] leading-6 text-[#D0D6F9] md:mb-40 md:px-[122px] md:text-base xl:mb-0 xl:px-0 xl:text-left xl:text-[18px] xl:leading-8 ">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+
+      <div className="group relative z-0 ">
+        <Link
+          href="/destination"
+          className="relative z-10 flex h-[150px] w-[150px] items-center justify-center rounded-full bg-white font-bellefair text-xl tracking-[1.25px] text-[#0B0D17] md:h-[242px] md:w-[242px] md:text-[32px] md:tracking-[2px] xl:h-[274px] xl:w-[274px]"
+        >
+          EXPLORE
+        </Link>
+        <div className=" bg-white opacity-0 xl:absolute xl:inset-0 xl:rounded-full xl:transition-transform xl:duration-700 xl:ease-in-out xl:group-hover:scale-[1.64] xl:group-hover:opacity-10"></div>
+      </div>
     </div>
   );
 };
